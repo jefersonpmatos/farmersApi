6a8e44c4b6110e0679e1e416aadd81d8297dfa24
Jeferson Pereira Matos <92311057+jefersonPMatos@users.noreply.github.com>
2024-02-01 10:36:50 -0300

    fix cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import router from "./routes/router";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.listen(3030, () => console.log("Server running on port 3030! 🚀"));
